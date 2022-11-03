b5bbfc16627d948b721e18be55fd8d2345c7ccfc
Kunal12m <117359850+Kunal12m@users.noreply.github.com>
2022-11-03 17:37:51 +0530

    Update Main.java

--- src/main/java/com/epam/genesis/pss/Main.java
+++ src/main/java/com/epam/genesis/pss/Main.java
@@ -3,5 +3,6 @@ package com.epam.genesis.pss;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Demo Test");
     }
 }
